8dc298c8bb6cb11a489301b5c1f106b76e38a523
Avnish Srivastava <avnishsrivastava@Avnishs-MacBook-Pro.local>
2018-11-06 02:55:33 +0530

    blink typewriter scroll

--- 8x8ledDisplay.ino
+++ 8x8ledDisplay.ino
@@ -1,7 +1,9 @@
 uint8_t rowPins[8] = {10, 7, 12, 6, 2, 13, A1,  5};
 uint8_t colPins[8] = {A3, A2, 3, 11, 4, 9, 8, A0};
 
-byte characters[][8] = {
+
+//https://rodrigosc.github.io/ArduinoLedMatrix/char_builder/builder.html
+byte alphabets[][8] = {
   {0x0, 0x0, 0x6, 0x9, 0x9, 0xf, 0x9, 0x9},             //a
   {0x0, 0x0, 0xf, 0x5, 0x5, 0x6, 0x5, 0xf},             //b
   {0x0, 0x0, 0x7, 0x8, 0x8, 0x8, 0x8, 0x7},             //c
@@ -27,99 +29,134 @@ byte characters[][8] = {
   {0x0, 0x0, 0x11, 0x11, 0x11, 0x15, 0x1b, 0x11},       //w
   {0x0, 0x0, 0x11, 0xa, 0x4, 0x4, 0xa, 0x11},           //x
   {0x0, 0x0, 0x11, 0xa, 0x4, 0x4, 0x4, 0x4},            //y
-  {0x0, 0x0, 0x1f, 0x1, 0x2, 0x4, 0x8, 0x1f}            //z
+  {0x0, 0x0, 0x1f, 0x1, 0x2, 0x4, 0x8, 0x1f},           //z
+  {0x81, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x81}      // space
 };
 
-byte* displayCharacter;
-int seconds;
+double speed;
 
 void setup() {
-
-  seconds = 2;
-  char text[] = "abcdefghijklmnopqrstuvwxyz";
-  initialize(text);
- 
+  speed = 1.5;                      //lower is higher scrolling/strobbing/blinking rate
+  initialize();
 }
+
 
 void loop() {
-
-//  render(len);
-
-}
-
-void continuos(char text[], int len) {
-  while (true) {
-    render(text, len);
-  }
-}
-
-void initialize(char text[]) {
-
-  seconds *= 60; //converting seconds to frames
-  for(int i=0; i<8; i++) {
-    pinMode(colPins[i],OUTPUT);
-    pinMode(rowPins[i],OUTPUT);
-  }
-
-  Serial.begin(9600);
-
-  int len = 0;
-  while (text[len] != '\0')
-    len++;
   
-  continuos(text, len);
-
+  blink("hello");
+  typewriter("Namaste");
+  scroll("hola");
 }
 
-void render(char text[], int len) {
-  
-  for (int character = 0; character < len; character++) {
-    displayCharacter = copy(characters[text[character] - 97]);
-    for (int frame = 1; frame <= seconds; frame++) {
-      if (frame % (seconds / 7) == 0) {
-        for (int row = 0; row < 8; row++) {
-          displayCharacter[row] = displayCharacter[row] << 1;
-        }
-      }
-//      strobing effect
-      if (frame > 80 && frame % 2 == 0) {
-        for (int i = 0; i <= (frame / 12)%8; i++) {
-          displayCharacter[i] = displayCharacter[i] >> 2;
-        }
-      }
-//      
-      multiplex(displayCharacter);
+void scroll(char* text) {
+  int len = length(text);
+
+  for (int index = 0; index < len; index++) {
+    byte* screen = screenShot(findAlphabet(text[index]));
+    for (int frame = 0, shiftFrameLimit = speed * 8; frame < speed * 60; frame++) {
+      if (frame % shiftFrameLimit == 0)
+        shiftFrameBy1(screen, 'l');
+
+      multiplex(screen);
     }
-    free(displayCharacter);
   }
-  
 }
 
-byte* copy(byte* original) {
-  int len = 8;
-  byte* copyArr = (byte*) malloc(sizeof(byte) * len);
+void typewriter(char* text) {
+  int len = length(text);
 
-  for (int i = 0; i < len; i++) {
-    copyArr[i] = original[i];
-  }
-  
-  return copyArr;
-}
+  for (int index = 0; index < len; index++) {
+    byte* screen = screenShot(findAlphabet(text[index]));
 
-void multiplex(byte* arr) {
-  
-  for(int row=0; row<8; row++) {
-      for(int k=0; k<8; k++) 
-        digitalWrite(colPins[k],HIGH);
-      
-      digitalWrite(rowPins[row], HIGH);
-      
-      for(int col=0; col<8; col++) {
-          digitalWrite(colPins[7-col], arr[row] & 1 << col ? LOW : HIGH);
+    char direction = 'l';
+    for (int frame = 0, shiftFrameLimit = speed * 10; frame < speed * 60; frame++) {
+      if (frame > speed * 40) {
+        direction = 'r';
+        shiftFrameLimit = speed+1;
       }
-      delay(1);
-      
-      digitalWrite(rowPins[row], LOW);
-   }
+
+      if (frame % shiftFrameLimit == 0)
+        shiftFrameBy1(screen, direction);
+
+      multiplex(screen);
+    }
+    free(screen);
+  }
 }
 
+void blink(char* text) {
+  int len = length(text);
+
+  for (int index = 0; index < len; index++) {
+    for (int frame = 0; frame < speed * 60; frame++) {
+      multiplex(findAlphabet(text[index]));
+    }
+    delay(speed * 100);
+  }
+}
+
+// This method prints the grid into the LED Matrix
+void multiplex(byte* grid) {
+  for (int row = 0; row < 8; row++) {
+    for (int col = 0; col < 8; col++)
+      digitalWrite(colPins[col], HIGH);
+
+    digitalWrite(rowPins[row], HIGH);
+
+    for (int col = 0; col < 8; col++)
+      digitalWrite(colPins[7-col], grid[row] & 1 << col ? LOW : HIGH);
+
+    delay(1);
+
+    digitalWrite(rowPins[row], LOW);
+  }
+}
+
+// Just initializing the pins as output
+void initialize() {
+  for(int i=0; i<8; i++) {
+      pinMode(colPins[i],OUTPUT);
+      pinMode(rowPins[i],OUTPUT);
+    }
+}
+
+int length(char* text) {
+  int len = 0;
+  while(text[len] != '\0')
+    len++;
+  return len;
+}
+
+byte* screenShot(byte* original) {
+  int len = 8;
+  byte* copy = (byte*) malloc(len);
+
+  for (int i = 0; i < len; i++) 
+    copy[i] = original[i];
+
+  return copy;
+}
+
+void shiftFrameBy1(byte* screen, char direction) {
+  for (int row = 0; row < 8; row++) { 
+    if (direction == 'l')
+      screen[row] <<= 1;
+    else
+      screen[row] >>= 1;
+  }
+}
+
+byte* findAlphabet(char character) {
+  if (character == ' ')
+    return alphabets[26];
+  else if (isUpper(character))
+    return alphabets[character - 65];
+  else
+    return alphabets[character - 97];
+}
+
+bool isUpper(char character) {
+  if (character >= 65 && character <= 90)
+    return true;
+  return false;
+}

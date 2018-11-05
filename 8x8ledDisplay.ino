174e80fc34faa01ef03538fd834fa2ea5f194d38
Avnish Srivastava <avnishsrivastava@Avnishs-MacBook-Pro.local>
2018-11-06 03:23:38 +0530

    minor changes

--- 8x8ledDisplay.ino
+++ 8x8ledDisplay.ino
@@ -44,11 +44,9 @@ void setup() {
 void loop() {
   
   blink("hello");
-  delay(1000);
   typewriter("Namaste");
-  delay(1000);
   scroll("hola");
-  delay(10000);
+  
 }
 
 void scroll(char* text) {
